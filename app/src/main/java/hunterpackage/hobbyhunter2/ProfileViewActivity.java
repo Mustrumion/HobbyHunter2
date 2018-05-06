ca11e46dd54b9ce949f0b3a34eee0ad3dbe23708
Mustrumion <bar.popiel@gmail.com>
2018-05-06 18:16:15 +0200

    Google maps integration
    Added map activity

--- app/src/main/java/hunterpackage/hobbyhunter2/ProfileViewActivity.java
+++ app/src/main/java/hunterpackage/hobbyhunter2/ProfileViewActivity.java
@@ -1,7 +1,11 @@
 package hunterpackage.hobbyhunter2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileViewActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class ProfileViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        configureMap();
+    }
+
+
+    private void configureMap(){
+        ImageView map = (ImageView) findViewById(R.id.miniMap);
+        map.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileViewActivity.this, MapActivity.class));
+            }
+        });
     }
 }
