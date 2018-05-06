c29b1ec299d0cb1d1a222710f88507dc46e9cdac
Mustrumion <bar.popiel@gmail.com>
2018-05-07 00:05:16 +0200

    Replacing placeholder message

--- app/src/main/java/hunterpackage/hobbyhunter2/MainActivity.java
+++ app/src/main/java/hunterpackage/hobbyhunter2/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No messages received.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
