a24ec4b529cc4c981f581a5b49d5c463c0728c03
Mustrumion <bar.popiel@gmail.com>
2018-05-24 21:03:55 +0200

    Registration cleanup and failure path
    Removed unnecessary redundant task. Added paths for failed registration.

--- app/src/main/java/hunterpackage/hobbyhunter2/RegisterActivity.java
+++ app/src/main/java/hunterpackage/hobbyhunter2/RegisterActivity.java
@@ -3,6 +3,8 @@ package hunterpackage.hobbyhunter2;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +30,7 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -62,7 +65,6 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -71,6 +73,7 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
     private View mProgressView;
     private View mLoginFormView;
     private ApiService mAPIService;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +84,7 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
         populateAutoComplete();
 
         mAPIService = ApiUtils.getAPIService();
+        layout = (LinearLayout)findViewById(R.id.registerlayout);
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordRepeatView = (EditText) findViewById(R.id.password_rep);
@@ -157,9 +161,6 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
 
         // Reset errors.
         mEmailView.setError(null);
@@ -206,8 +207,10 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            sendUser(user);
         }
     }
     private boolean isEmailValid(String email) {
@@ -314,80 +317,25 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
         mAPIService.saveUser(user).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-
+                showProgress(false);
                 if(response.isSuccessful()) {
-                    showResponse(response.body().toString());
-                    Log.i("asd", "post submitted to API." + response.body().toString());
+                    Snackbar.make(layout, "Registration successful.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                }
+                else{
+                    Snackbar.make(layout, "User with that address already exists, try something more creative.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
                 }
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
-                Log.e("asd", "Unable to submit post to API.");
+                showProgress(false);
+                Snackbar.make(layout, "Registration request failed, miserably.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-    }
-
-    public void showResponse(String response) {
-        /*if(mResponseTv.getVisibility() == View.GONE) {
-            mResponseTv.setVisibility(View.VISIBLE);
-        }
-        mResponseTv.setText(response);*/
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            User user = new User();
-            user.setEmail(mEmail);
-            user.setPassword(mPassword);
-            sendUser(user);
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
     }
 }
 
